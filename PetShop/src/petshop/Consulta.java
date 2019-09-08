d4f7456429b2e2b47a68db869058a8d291ee35a9
edugu <edugu@LAPTOP-7BIUVP4S>
2019-09-07 23:36:13 -0300

    Teste commit 2.

--- PetShop/src/petshop/Consulta.java
+++ PetShop/src/petshop/Consulta.java
@@ -2,10 +2,8 @@ package petshop;
 
 import java.util.*;
 
-public class PetShop {
-
-    public static void main(String[] args) {
+public class Consulta {
+        public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }
